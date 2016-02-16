de1a320ee4e27551f9b87e6c2c894ad408d7db53
misterjunio <ruca.33.maia@gmail.com>
2016-02-16 17:33:19 +0000

    Finish logsh log in/log out; Saída da FhP 16/02

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,6 +21,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
-    assert_not flash.:success
+    assert is_logged_in?
   end
 end
