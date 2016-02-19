e8a51acf658b34631b6c4d9399bac34bae145438
Mister Júnio <ruca.33.maia@gmail.com>
2016-02-19 14:30:17 +0000

    Forgot the titles

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("About"), "About | Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Contact"), "Contact | Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Signup"), "Signup | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title,         "Incubacona"
+    assert_equal full_title("Help"), "Help | Incubacona"
+    assert_equal full_title("About"), "About | Incubacona"
+    assert_equal full_title("Contact"), "Contact | Incubacona"
+    assert_equal full_title("Signup"), "Signup | Incubacona"
   end
 end
